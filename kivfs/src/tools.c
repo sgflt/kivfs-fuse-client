00679b2f9a319843b95bc98c6144eeea930c04a7
Lukáš Kvídera <kvideral@students.zcu.cz>
2013-10-12 21:30:51 +0200

    New tools.c
    Can do some awesome stuff.

---
+++ kivfs/src/tools.c
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------
+ * tools.c
+ *
+ *  Created on: 12. 10. 2013
+ *      Author: Lukáš Kvídera, A11B0421P
+ *	   Version: 0.0
+ ---------------------------------------------------------------------------*/
+
+#include <unistd.h>
+#include <errno.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mkdirs(const char *path){
+
+	char *dir = strdup( path );
+
+	/* dirname changes original string */
+	dirname( dir );
+
+	if( access( dir, F_OK ) == -1 ){
+		mkdirs( dir );
+		if( mkdir( dir, S_IRWXU) ){
+			return -errno;
+		}
+	}
+
+	free( dir );
+	return 0;
+}

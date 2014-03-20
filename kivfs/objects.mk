f1d0619cf451de0df3c21fa7a1c6a6f67e64a37a
Lukáš Kvídera <kvideral@students.zcu.cz>
2014-03-20 12:44:52 +0100

    Update: Add heimdal-dev package.

--- kivfs/objects.mk
+++ kivfs/objects.mk
@@ -1,4 +1,4 @@
 USER_OBJS :=
 
-LIBS := `pkg-config fuse sqlite3 libssl krb5 --libs` -lulockmgr -lpthread -lgssapi -lkivfscore
+LIBS := `pkg-config fuse sqlite3 libssl --libs` -lulockmgr -lpthread -lgssapi -lkivfscore
 
